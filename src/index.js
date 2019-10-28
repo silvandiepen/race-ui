2b38832a57169f157745402ce2b65a8d658d7cbe
Sil van Diepen <me@silvandiepen.com>
2019-10-28 11:27:37 +0100

    test it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const paddle = document.querySelector(".paddle");
 
+console.log(paddle);
+
 function handleOrientation(event) {
+  console.log("does it");
+  alert("yeah");
   const orientation = {
     absolute: event.absolute,
     alpha: event.alpha,
@@ -10,12 +14,28 @@ function handleOrientation(event) {
 
   console.log(orientation);
 
-  paddle.innerHTML = `<p>${orientation.absolute}</p><p>${orientation.alpha}</p><p>${orientation.beta}</p><p>${orientation.gamma}</p>`;
+  paddle.innerHTML = `<p>${orientation.beta}</p>`;
+
+  rotation = 0;
+  if (event.beta > 90) rotation = 90;
+  else if (event.beta < 0) rotation = 0;
+
+  rotation = Math.round((255 / 90) * rotation);
+
+  paddle.setAttribute("data-rotation", rotation);
+  paddle.innerHTML = `<h2>${rotation}</h2>`;
 
   // Do stuff with the new orientation data
 }
+if (window.DeviceOrientationEvent) {
+  // setup real compass thing, with event.alpha
+  document.body.innerHTML = "haz!";
+} else {
+  // setup some mouse following hack
+  document.body.innerHTML = "nope";
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("Yeah.. lets go!");
+  console.log("Yeah.. lets go!  fdasfas");
   window.addEventListener("deviceorientation", handleOrientation, true);
 });
