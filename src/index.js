faeb6a8260ab9621d48ab691b5d66feeed54cc26
Sil van Diepen <me@silvandiepen.com>
2019-10-28 14:43:12 +0100

    add mouse move option

--- src/index.js
+++ src/index.js
@@ -28,14 +28,16 @@ function sendData(msg) {
 function startGame() {
   button.addEventListener("click", () => {
     button.classList.add("button--clicked");
-    if (typeof DeviceOrientationEvent.requestPermission === "function") {
-      DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === "granted") {
-            startPedal();
-          }
-        })
-        .catch(console.error);
+    if (
+      DeviceOrientationEvent &&
+      DeviceOrientationEvent.requestPermission &&
+      typeof DeviceOrientationEvent.requestPermission === "function"
+    ) {
+      DeviceOrientationEvent.requestPermission().then(permissionState => {
+        if (permissionState === "granted") {
+          startPedal();
+        }
+      });
     } else {
       startPedal();
     }
@@ -43,6 +45,7 @@ function startGame() {
 }
 
 function handleOrientation(e) {
+  console.log(e);
   // Check if rotation is between 90 and 0 and Round it off to a number between 0 and 255.
   const rotation = Math.round(
     ((100 / 90) * (e.beta > 90 ? 90 : e.beta < 0 ? 0 : e.beta) - 100) * -1
@@ -54,10 +57,30 @@ function handleOrientation(e) {
   sendData(rotation);
 }
 
+function handleMouseMove(e) {
+  // if out of the container, don't do anything.
+  let PS = pedal.getBoundingClientRect();
+  if (
+    e.clientY < PS.top ||
+    e.clientX < PS.left ||
+    e.clientX > PS.left + PS.width ||
+    e.clientY > PS.top + PS.height
+  ) {
+    return;
+  } else {
+    let value = Math.round(
+      ((100 / PS.height) * (e.clientY - PS.top) - 100) * -1
+    );
+    pedal.setAttribute("data-rotation", value);
+    pedal.innerHTML = `<h2>${value}</h2>`;
+    pedal.style.setProperty("--rotation", value);
+    sendData(value);
+  }
+}
+
 function startPedal() {
-  //   setTimeout(() => {
   document.body.setAttribute("data-stage", 1);
-  //   }, 0);
+
   setTimeout(() => {
     document.body.setAttribute("data-stage", 2);
   }, 1000);
@@ -69,10 +92,19 @@ function startPedal() {
     document.body.classList.add("initialized");
 
     if (window.DeviceOrientationEvent) {
+      console.log("initializedd deviceorientation");
       window.addEventListener("deviceorientation", e => {
         handleOrientation(e);
       });
+      // MouseMove functionality
+      document.addEventListener("mousemove", e => {
+        handleMouseMove(e);
+      });
     } else {
+      // MouseMove functionality
+      document.addEventListener("mousemove", e => {
+        handleMouseMove(e);
+      });
       console.log(" go do something with the pointer");
     }
   }, 3000);
