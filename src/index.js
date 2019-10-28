2cabf47944aab367edc2f5b0e890a1ac0a6ce72b
Sil van Diepen <me@silvandiepen.com>
2019-10-28 11:39:48 +0100

    yeah

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 const paddle = document.querySelector(".pedal");
 
-console.log(paddle);
-
 function handleOrientation(event) {
-  const orientation = {
-    absolute: event.absolute,
-    alpha: event.alpha,
-    beta: event.beta,
-    gamma: event.gamma
-  };
+  //   const orientation = {
+  //     absolute: event.absolute,
+  //     alpha: event.alpha,
+  //     beta: event.beta,
+  //     gamma: event.gamma
+  //   };
 
-  paddle.innerHTML = `<p>${orientation.beta}</p>`;
-
-  rotation = 0;
-  if (event.beta > 90) rotation = 90;
-  else if (event.beta < 0) rotation = 0;
-
-  rotation = Math.round((255 / 90) * rotation);
+  // Check if rotation is between 90 and 0 and Round it off to a number between 0 and 255.
+  rotation = Math.round(
+    (255 / 90) * (event.beta > 90 ? 90 : event.beta < 0 ? 0 : event.beta)
+  );
 
   paddle.setAttribute("data-rotation", rotation);
   paddle.innerHTML = `<h2>${rotation}</h2>`;
