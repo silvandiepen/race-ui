61afe809542701290126e21ded53049d805292d4
Sil van Diepen <me@silvandiepen.com>
2019-10-28 13:08:38 +0100

    with button:

--- src/index.js
+++ src/index.js
@@ -25,6 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 button.addEventListener("click", () => {
+  button.classList.add("button--clicked");
+  document.body.classList.add("initialized");
   if (typeof DeviceOrientationEvent.requestPermission === "function") {
     alert("yeah gonna check");
     DeviceOrientationEvent.requestPermission()
