418d0a2c065ceb8326bc30ded3a2e109adb0a683
Sil van Diepen <me@silvandiepen.com>
2019-10-28 11:57:41 +0100

    0 to a 100

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ function handleOrientation(event) {
 
   // Check if rotation is between 90 and 0 and Round it off to a number between 0 and 255.
   const rotation = Math.round(
-    (255 / 90) * (event.beta > 90 ? 90 : event.beta < 0 ? 0 : event.beta)
+    ((100 / 90) * (event.beta > 90 ? 90 : event.beta < 0 ? 0 : event.beta) -
+      100) *
+      -1
   );
 
   paddle.setAttribute("data-rotation", rotation);
